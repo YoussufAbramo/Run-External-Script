37aa12e937eec132c2c1636e6b0b88bd8e12bfc0
Youssuf Abramo <84648094+YoussufAbramo@users.noreply.github.com>
2024-01-07 18:12:48 +0400

    config file update

--- config.php
+++ config.php
@@ -1,31 +1,24 @@
 <?php
 ##############################
-########### Script ###########
+####### Script Config ########
 ##############################
 $esp_config = [
-  'main_url' => 'google.com-globalseo.com',
-  'script_directory' => $esp_config['main_url'] . '/s/',
-  'script_name' => 'tag-manager-script.php',
+  'main_url' => 'domain-name.com', # Replace With Your Domain Name
+  'script_directory' => $esp_config['main_url'] . '/s/', # Replace With Your Correct Script Directory
+  'script_name' => 'tag-manager-script.php', # Replace With Your Correct Script Name
   'full_script_url' => $personData['script_directory'] . $personData['script_name'],
-  'email_address' => "customer_support@" . $esp_config['main_url']
+  'email_address' => "customer_support@" . $esp_config['main_url'] # Replace With Your Preffered Email Address
 ];
-
-// To Use It:
-// echo $personData['full_script_url'];
-
 
 ##############################
 ########### Email ############
 ##############################
 // Recipient email address
 $email_address = $esp_config['email_address'];
-
 // Subject of the email
 $subject = "Test Email";
-
 // Message body
 $message = "This is a test email sent using PHP.";
-
 // Additional headers
 $headers = "From: " . $email_address . "\r\n";
 $headers .= "Reply-To: " . $email_address . "\r\n";
